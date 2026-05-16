77da7331744507d696cfa8ec44c31f36d57da915
jeimimarilin-hue <jeimimarilin@gmail.com>
2026-05-16 16:25:07 -0400

    12

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,15 +2,17 @@ document.addEventListener("DOMContentLoaded", () => {
     const menuToggle = document.getElementById("menu-toggle");
     const navigation = document.querySelector(".navigation");
 
-    menuToggle.addEventListener("click", () => {
-        navigation.classList.toggle("open");
-        
-        if (navigation.classList.contains("open")) {
-            menuToggle.innerHTML = "&times;";
-        } else {
-            menuToggle.innerHTML = "&#9776;";
-        }
-    });
+    if (menuToggle && navigation) {
+        menuToggle.addEventListener("click", () => {
+            navigation.classList.toggle("open");
+            
+            if (navigation.classList.contains("open")) {
+                menuToggle.innerHTML = "&times;";
+            } else {
+                menuToggle.innerHTML = "&#9776;";
+            }
+        });
+    }
 
     const currentYearSpan = document.getElementById("currentyear");
     if (currentYearSpan) {

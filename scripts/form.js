cd29f42093a034060c7a485c06ef069adb599db2
jeimimarilin-hue <jeimimarilin@gmail.com>
2026-06-06 21:39:09 -0400

    53

--- scripts/form.js
+++ scripts/form.js
@@ -1,13 +1,14 @@
-const products = [
-  { id: "fc-1020", name: "flux capacitor", averagerating: 4.5 },
-  { id: "bc-1998", name: "power converters", averagerating: 3.9 },
-  { id: "nv-1987", name: "vaporizer", averagerating: 5.0 },
-  { id: "jj-1969", name: "warp equalizer", averagerating: 4.2 },
-  { id: "lr-1933", name: "time machine", averagerating: 4.7 }
-];
+document.addEventListener("DOMContentLoaded", function() {
+    const products = [
+      { id: "fc-1020", name: "flux capacitor", averagerating: 4.5 },
+      { id: "bc-1998", name: "power converters", averagerating: 3.9 },
+      { id: "nv-1987", name: "vaporizer", averagerating: 5.0 },
+      { id: "jj-1969", name: "warp equalizer", averagerating: 4.2 },
+      { id: "lr-1933", name: "time machine", averagerating: 4.7 }
+    ];
 
-document.addEventListener("DOMContentLoaded", () => {
     const productSelect = document.getElementById("product-select");
+    
     if (productSelect) {
         products.forEach(product => {
             const option = document.createElement("option");
